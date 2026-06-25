6b8b1affe5265e226627c18a6b5d241023722b1e
no0000llkl <noname0120010@gmail.com>
2026-06-26 00:49:44 +0300

    Add SecondPage component with placeholder content

---
+++ client/client/src/pages/SecondPage.jsx
@@ -0,0 +1,10 @@
+function SecondPage() {
+  return (
+    <div className="page">
+      <h2>Вторая страница</h2>
+      <p>Это вторая страница-заглушка. Здесь позже можно добавить данные.</p>
+    </div>
+  );
+}
+
+export default SecondPage;
