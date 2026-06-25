7dab570e7277a8665e60e276a9f21859a200ad9b
no0000llkl <noname0120010@gmail.com>
2026-06-26 01:34:58 +0300

    Update App.jsx

--- client/client/src/App.jsx
+++ client/client/src/App.jsx
@@ -1,21 +1,31 @@
+import { useState } from "react";
 import "./App.css";
 
 function App() {
+  const [page, setPage] = useState("first");
+
   return (
     <div className="app">
       <h1>Моё React-приложение</h1>
 
-      <div className="pages">
+      <div className="menu">
+        <button onClick={() => setPage("first")}>Первый лист</button>
+        <button onClick={() => setPage("second")}>Второй лист</button>
+      </div>
+
+      {page === "first" && (
         <div className="page">
           <h2>Первый лист</h2>
           <p>Это первая страница-заглушка.</p>
         </div>
+      )}
 
+      {page === "second" && (
         <div className="page">
           <h2>Второй лист</h2>
           <p>Это вторая страница-заглушка.</p>
         </div>
-      </div>
+      )}
     </div>
   );
 }
