9d78e348c100dfc84e3c1e86942cf5ffe401057f
no0000llkl <noname0120010@gmail.com>
2026-06-26 00:49:01 +0300

    Add FirstPage component with placeholder content

---
+++ client/client/src/pages/FirstPage.jsx
@@ -0,0 +1,10 @@
+function FirstPage() {
+  return (
+    <div className="page">
+      <h2>Первая страница</h2>
+      <p>Это страница-заглушка. Здесь позже будет основной контент.</p>
+    </div>
+  );
+}
+
+export default FirstPage;
